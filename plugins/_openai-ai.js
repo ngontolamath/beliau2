19cbf657a4b98882ab1bbc75f2822c53e7bf090c
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2024-01-11 08:46:06 +0700

    Update _openai-ai.js

--- plugins/_openai-ai.js
+++ plugins/_openai-ai.js
@@ -1,20 +1,41 @@
-var fetch = require('node-fetch');
-var handler = async (m, {
- text, 
- usedPrefix, 
- command
- }) => {
-if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* Siapa presiden Indonesia? `
-try {
-  await m.reply(wait)
-  var apii = await fetch(`https://api.betabotz.eu.org/api/search/openai-chat?text=${text}&apikey=${btc}`)
-  var res = await apii.json()
-  await m.reply(res.message)
-} catch (err) {
-  console.error(err)
-  throw "Terjadi kesalahan dalam menjawab pertanyaan"
+const fetch = require('node-fetch');
+
+const handler = async (m, { text, conn, usedPrefix, command }) => {
+  if (!text && !(m.quoted && m.quoted.text)) {
+    throw `Please provide some text or quote a message to get a response.`;
+  }
+
+  if (!text && m.quoted && m.quoted.text) {
+    text = m.quoted.text;
+  }
+
+  try {
+    await m.reply('Thinking....');
+    const prompt = encodeURIComponent(text);
+
+    const apiEndpoint = `https://ultimetron.guruapi.tech/gpt3?prompt=${text}`;
+
+    try {
+      let response = await fetch(apiEndpoint);
+      let data = await response.json();
+      let result = data.completion;
+
+      if (!result) {
+        throw new Error('No valid JSON response from the API');
+      }
+
+      await m.reply(result);
+    } catch (error) {
+      console.error('Error from the API:', error);
+      throw "An error occurred while processing the request.";
+    }
+
+  } catch (error) {
+    console.error('Error:', error);
+    throw `*ERROR*`;
+  }
 }
-}
+
 handler.command = handler.help = ['ai','openai','chatgpt'];
 handler.tags = ['tools'];
 handler.premium = false
