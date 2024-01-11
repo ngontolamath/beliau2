58840004051b6c459d7765a0419f8c6a164e6ac6
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2024-01-11 08:37:41 +0700

    Update info-sewa.js

--- plugins/info-sewa.js
+++ plugins/info-sewa.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command }) => {
-    let buffer = await fetch(`https://telegra.ph/file/1c53a85e78e81e2aaad9c.jpg`).then(res => res.buffer())
-    conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Jika telah melakukan pembayaran silahkan kirimkan bukti pembayaran ke WhatsApp Owner.*`, m)
+    let buffer = await fetch(`https://i.ibb.co/G7kHzy0/IMG-20240111-083348.jpg`).then(res => res.buffer())
+    conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Jika telah melakukan pembayaran silahkan kirimkan bukti screenshot pembayaran ke WhatsApp Owner.*`, m)
 }
 
 handler.help = handler.command = ['donasi','donate','sewa','sewabot','belibot']
