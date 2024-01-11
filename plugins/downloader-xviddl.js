96c5a4740a773d787413d5d3a497b7bd9c679fb0
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2024-01-11 13:24:28 +0700

    Update downloader-xviddl.js

--- plugins/downloader-xviddl.js
+++ plugins/downloader-xviddl.js
@@ -6,7 +6,7 @@ var handler = async (m, {
  }) => {
 if (!text) throw 'Masukkan Query Link!'
  try {
-let anu = await fetch(`https://api.botcahx.eu.org/api/download/xvideosdl?url=${text}&apikey=u3PIc2hC`)
+let anu = await fetch(`https://api.botcahx.eu.org/api/download/xvideosdl?url=${text}&apikey=${lann}`)
 let hasil = await anu.json() 
 
 conn.sendFile(m.chat, hasil.result.url, 'eror.mp4', 'Cabul anjg', m)
